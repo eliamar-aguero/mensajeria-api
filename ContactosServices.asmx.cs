74328ea9a6a338cb7b320f9e179bddd8a3ded43b
Eliamar Aguero <eliamar.aguero@gmail.com>
2020-03-12 21:41:34 -0600

    - crear clase para las operaciones del crud - crear método de retornar lista - crear método de retornar record único - crear método de eliminar registro - crear método para crear registro nuevo

--- ContactosServices.asmx.cs
+++ ContactosServices.asmx.cs
@@ -1,46 +1,105 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
+﻿using System.Data;
 using System.Web.Services;
-using System.Configuration;
 
-namespace mensajeria_ws
-{
+namespace mensajeria_ws {
     /// <summary>
-    /// Descripción breve de WebService1
+    /// Servicio web para aplicación de administración de contactos y mensajes a contactos
     /// </summary>
     [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
     // [System.Web.Script.Services.ScriptService]
-    public class WebService1 : System.Web.Services.WebService
-    {
-
+    public class WebService1 : WebService {
         [WebMethod]
-        public string HelloWorld()
-        {
-            return "Hola a todos";
+        public DataSet GetAllContacts() {
+            string sql = "SELECT id, nombre FROM contactos;";
+            return CrudClass.getAllRecords(sql);
         }
 
+        [WebMethod]
+        public DataSet GetSingleContact(int id) {
+            string sql = "SELECT * FROM contactos where id = @id;";
+            return CrudClass.getSingleRecord(sql, id);
+        }
 
         [WebMethod]
-        public DataSet Obtener_dato(string id)
-        {
-            string conString = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
-            DataSet ds = new DataSet();
-            SqlConnection con = new SqlConnection(conString);
-            SqlCommand sql = new SqlCommand("SELECT contactos.nombre, contactos.tel_movil, mensajes.mensaje FROM contactos INNER JOIN mensajes ON contactos.id = mensajes.contactos_id; ", con);
-           // sql.Parameters.Add("@id", SqlDbType.NVarChar);
-           // sql.Parameters[0].Value = id;
-            SqlDataAdapter da = new SqlDataAdapter(sql);
-            da.Fill(ds);
-
-            return ds;
+        public DataSet DeleteContact(int id) {
+            string sql = "DELETE FROM contactos where id = @id;";
+            return CrudClass.deleteRecord(sql, id);
         }
-        
+
+        [WebMethod]
+        public DataSet CreateContact(
+            string name,
+            string organization,
+            string position,
+            string storeType,
+            string email,
+            string showAs,
+            string website,
+            string imAdress,
+            string phoneWork,
+            string phoneHome,
+            string faxWork,
+            string phoneMobile,
+            string workAddress,
+            int correspondence,
+            string notes
+            ) {
+            string sql = @"INSERT INTO contactos(
+                nombre,
+                organizacion,
+                puesto,
+                archivar_como_a,
+                email,
+                mostrar_como,
+                pagina_web,
+                direccion_im,
+                tel_trabajo,
+                tel_particular,
+                fax_trabajo,
+                tel_movil,
+                direccion_trabajo,
+                direccion_correspondencia,
+                notas
+            )
+            VALUES(
+                @name,
+                @organization,
+                @position,
+                @storeType,
+                @email,
+                @showAs,
+                @website,
+                @imAdress,
+                @phoneWork,
+                @phoneHome,
+                @faxWork,
+                @phoneMobile,
+                @workAddress,
+                @correspondence,
+                @notes
+            )";
+            return CrudClass.createRecord(
+                sql,
+                name,
+                organization,
+                position,
+                storeType,
+                email,
+                showAs,
+                website,
+                imAdress,
+                phoneWork,
+                phoneHome,
+                faxWork,
+                phoneMobile,
+                workAddress,
+                correspondence,
+                notes
+            );
+        }
+
     }
 }
---
+++ CrudClass.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace mensajeria_ws {
+    public class CrudClass {
+        private static string conString = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+        private static SqlConnection con = new SqlConnection(conString);
+
+        public static DataSet getAllRecords(string query) {
+            SqlCommand sql = new SqlCommand(query, con);
+            SqlDataAdapter da = new SqlDataAdapter(sql);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+
+        public static DataSet getSingleRecord(string query, int id) {
+            SqlCommand sql = new SqlCommand(query, con);
+            sql.Parameters.Add("@id", SqlDbType.NVarChar);
+            sql.Parameters[0].Value = id;
+            SqlDataAdapter da = new SqlDataAdapter(sql);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+
+        public static DataSet deleteRecord(string query, int id) {
+            SqlCommand sql = new SqlCommand(query, con);
+            sql.Parameters.Add("@id", SqlDbType.NVarChar);
+            sql.Parameters[0].Value = id;
+            SqlDataAdapter da = new SqlDataAdapter(sql);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+
+        public static DataSet createRecord(
+            string query,
+            string name,
+            string organization,
+            string position,
+            string storeType,
+            string email,
+            string showAs,
+            string website,
+            string imAdress,
+            string phoneWork,
+            string phoneHome,
+            string faxWork,
+            string phoneMobile,
+            string workAddress,
+            int correspondence,
+            string notes
+        ) {
+            SqlCommand sql = new SqlCommand(query, con);
+            sql.Parameters.Add("@name", SqlDbType.NVarChar);
+            sql.Parameters[0].Value = name;
+            sql.Parameters.Add("@organization", SqlDbType.NVarChar);
+            sql.Parameters[1].Value = organization;
+            sql.Parameters.Add("@position", SqlDbType.NVarChar);
+            sql.Parameters[2].Value = position;
+            sql.Parameters.Add("@storeType", SqlDbType.NVarChar);
+            sql.Parameters[3].Value = storeType;
+            sql.Parameters.Add("@email", SqlDbType.NVarChar);
+            sql.Parameters[4].Value = email;
+            sql.Parameters.Add("@showAs", SqlDbType.NVarChar);
+            sql.Parameters[5].Value = showAs;
+            sql.Parameters.Add("@website", SqlDbType.NVarChar);
+            sql.Parameters[6].Value = website;
+            sql.Parameters.Add("@imAdress", SqlDbType.NVarChar);
+            sql.Parameters[7].Value = imAdress;
+            sql.Parameters.Add("@phoneWork", SqlDbType.NVarChar);
+            sql.Parameters[8].Value = phoneWork;
+            sql.Parameters.Add("@phoneHome", SqlDbType.NVarChar);
+            sql.Parameters[9].Value = phoneHome;
+            sql.Parameters.Add("@faxWork", SqlDbType.NVarChar);
+            sql.Parameters[10].Value = faxWork;
+            sql.Parameters.Add("@phoneMobile", SqlDbType.NVarChar);
+            sql.Parameters[11].Value = phoneMobile;
+            sql.Parameters.Add("@workAddress", SqlDbType.NVarChar);
+            sql.Parameters[12].Value = workAddress;
+            sql.Parameters.Add("@correspondence", SqlDbType.TinyInt);
+            sql.Parameters[13].Value = correspondence;
+            sql.Parameters.Add("@notes", SqlDbType.NVarChar);
+            sql.Parameters[14].Value = notes;
+            SqlDataAdapter da = new SqlDataAdapter(sql);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+    }
+}
